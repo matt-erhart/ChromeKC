942b2edaa2dadacef3749b6ef6bc7da840f1513a
Matt Erhart <mattjerhart@gmail.com>
2017-05-26 18:31:24 -0700

    screenshot with scaled preview

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -2,7 +2,54 @@ import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
 
+class CanvasComponent extends React.Component {
 
+  
+  constructor(props) {
+    super(props);
+  }
+
+  updateCanvas = () => {
+      console.log('update canvas')
+        const ctx = this.refs.canvas.getContext('2d');
+        if (this.props.img !== undefined) {
+                var img = new Image;
+                img.src = this.props.img;
+                img.onload = function () {
+                  var height = img.height;
+                  var width = img.width;
+                  console.log(img.width, img.height)
+                  // ctx.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
+                  //  0, 0, 500, 300);
+                  var canvas = ctx.canvas ;
+                  var hRatio = canvas.width / img.width;
+                  var vRatio = canvas.height / img.height;
+                  var ratio = Math.min(hRatio, vRatio);
+                  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+                  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+                  ctx.clearRect(0, 0, canvas.width, canvas.height);
+                  ctx.drawImage(img, 0, 0, img.width, img.height,
+                    centerShift_x, centerShift_y, img.width * ratio, img.height * ratio);  
+                }
+                
+        }
+        
+
+    }
+    componentDidMount() {
+        this.updateCanvas();
+    }
+    componentDidUpdate() {
+        this.updateCanvas();
+    }
+    
+
+    render() {
+        return (
+            <canvas ref="canvas" width={500} height={300}/>
+        );
+    }
+}
 
 export default class Root extends Component {
 
@@ -38,6 +85,10 @@ export default class Root extends Component {
       console.log('root / popup response',response);
     });
   });  
+  chrome.tabs.captureVisibleTab(null, {format: "png"}, (data) => {
+    console.log('png')
+      this.setState({img: data})
+   })
 
 }
 
@@ -45,6 +96,7 @@ export default class Root extends Component {
     const { store } = this.props;
     return (
       <div>
+        <CanvasComponent img={this.state.img}></CanvasComponent>
         <div>{this.state.title}</div>
         <div>{this.state.url}</div>
         <textarea name="" id="" cols="60" rows="10" placeholder='remark'></textarea> <br/>
