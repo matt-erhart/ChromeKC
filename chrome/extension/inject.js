e1f76da1131e45df8f3113fdec02ee471cf67726
Matt Erhart <mattjerhart@gmail.com>
2017-06-01 15:55:01 -0700

    select working better

--- chrome/extension/inject.js
+++ chrome/extension/inject.js
@@ -18,32 +18,35 @@ class InjectApp extends Component {
     let body = document.getElementsByTagName("body")[0];
 
     const dragSelect$ = mouseDown$.mergeMap(downData => {
-      body.style.userSelect = 'none'
-      this.setState({display: 'block', dragSelect: {x: null, y: null, width: null, height: null}})
+      this.setState({dragSelect: {x: null, y: null, width: null, height: null}})
       return mouseMove$.takeUntil(mouseUp$)
         .do(moveData => {
-          
-          const {offsetX, offsetY, clientX, clientY} = downData;
-                    const w = (moveData.clientX-clientX); //dragging right positive
-                    const h = ( moveData.clientY-clientY); //dragging down positive
-                    const x = w < 0? clientX + w: clientX; 
-                    const y = h < 0? clientY + h: clientY;
-                    const width = Math.abs(w);
-                    const height = Math.abs(h);
-                    const dragSelect = {x, y, width, height}
-                    this.setState({dragSelect})
-                    console.log(dragSelect)
+            const {offsetX, offsetY, clientX, clientY} = downData;
+            const w = (moveData.clientX-clientX); //dragging right positive
+            const h = ( moveData.clientY-clientY); //dragging down positive
+
+            const x = w < 0? clientX + w: clientX; 
+            const y = h < 0? clientY + h: clientY;
+            const width = Math.abs(w);
+            const height = Math.abs(h);
+            const dragSelect = {x, y, width, height}
+            if (width+height > 8) {
+                body.style.userSelect = 'none'
+                this.setState({dragSelect})
+                this.setState({display: 'block'})
+                console.log(dragSelect)
+            }
         }).finally(moveData => {body.style.userSelect='auto'; this.setState({display: 'none'}); console.log('finally',moveData)})
     }).subscribe()
   }
 
   render() {
     const {x,y,width,height} = this.state.dragSelect;
-    return (
-          // <div style={{position:'fixed', color: 'grey', left: x, top: y, width: width, height: height}}></div>
-          // <div style={{position:'absolute', outlineColor: 'blue',backgroundColor:'black', left: x, top: y, width: width, height: height}}>&nbsp;</div>
-          <svg style={{position: 'fixed', left: 0, top:0, z: 1, display: this.state.display }} x={0} y={0} width={window.innerWidth} height={window.innerHeight} ><rect stroke={'black'} x={x} y={y} width={width} height={height} fill={'none'}></rect></svg>
-    );
+    let element = null;
+    if (this.state.display === 'block'){
+       element =  <svg style={{position: 'fixed', left: 0, top:0, z: 1, display: this.state.display }} x={0} y={0} width={window.innerWidth} height={window.innerHeight} ><rect stroke={'black'} x={x} y={y} width={width} height={height} fill={'none'}></rect></svg>
+    } 
+    return element;
   }
 }
 
